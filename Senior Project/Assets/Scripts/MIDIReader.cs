bf01cc05de6effaac9a83bd97234396f3619301b
Gelzone Unsas <gelzonexunsas@gmail.com>
2022-11-01 12:50:17 -0700

    MIDI in progress

--- Senior Project/Assets/Scripts/MIDIReader.cs
+++ Senior Project/Assets/Scripts/MIDIReader.cs
@@ -12,25 +12,79 @@ using UnityEngine;
 
 public class MIDIReader : MonoBehaviour
 {
+    /*
+
+     ----------------x----x-----x-----x-----                                > piano (64)
+     ------x-x---x-----x--x--------x--x-----                > guitar (60)   > piano (63)
+     --x-x-----x-x-----------x--x--x--x-----   > drum (57)  > guitar (59)   > piano (62)
+     x---x---x-x-x-x-x-x--x--x--x--x--x-----   > drum (56)  > guitar (58)   > piano (61)
+     1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+
+    */
+
     TempoMap tempoMap;
     int BEATS_PER_BAR;
     int SPOTS_PER_BEAT = 4;
+
+    MidiFile midiFile = MidiFile.Read("Assets/Music/test_beatmap.mid");
+
     List<NoteElement> trackInfo = new List<NoteElement>();
+    uint[] BinaryTrack;
 
     struct NoteElement
     {
         public byte number;
-        public String pos;
+        /* 
+         DRUM: 56 (G), 57 (A)
+         GUITAR: 58 (A#), 59 (B), 60 (C)
+         PIANO: 61 (C#), 62(D), 63 (D#), 64 (E)
+         */
+        public ushort[] pos;
         public long Length;
         public byte Velocity;
     }
 
     void Start()
     {
-        /*        InitializeOutputDevice();*/
-        var midiFile = MidiFile.Read("Assets/Music/test_beatmap.mid");
+        InitNoteElement();
+        InitBinaryTrack();
+    }
+
+    void InitNoteElement()
+    {
+        
         var notes = midiFile.GetNotes();
         tempoMap = midiFile.GetTempoMap();
+        
+        foreach (var note in notes)
+        {
+            NoteElement newNote = new NoteElement();
+            // track number and instrument type
+            newNote.number = note.NoteNumber;
+            Debug.Log(newNote.number);
+            // note position
+            var pos = note.TimeAs(TimeSpanType.BarBeatTicks, tempoMap).ToString();
+            var temp = pos.Split(".");
+            newNote.pos = new ushort[3];
+            int i = 0;
+            foreach (var num in temp)
+            {
+                newNote.pos[i] = Convert.ToUInt16(num);
+                i++;
+            }
+            Debug.Log("Bar: " + newNote.pos[0]);
+            Debug.Log("Beat: " + newNote.pos[1]);
+            Debug.Log("Ticks: " + newNote.pos[2]);
+            // note length for press & hold
+            newNote.Length = note.Length;
+            // note types
+            newNote.Velocity = note.Velocity;
+            trackInfo.Append(newNote);
+        }
+    }
+
+    void InitBinaryTrack() {
+
         var firstTrackChunk = midiFile.GetTrackChunks().First();
         var firstTimeSignatureEvent = firstTrackChunk
             .Events
@@ -43,18 +97,7 @@ public class MIDIReader : MonoBehaviour
         var totalBars = barBeatTimeOfLastEvent.Bars;
         if (barBeatTimeOfLastEvent.Beats > 0)
             totalBars = barBeatTimeOfLastEvent.Bars + 1;
-        foreach (var note in notes)
-        {
-            NoteElement newNote = new NoteElement();
-            // track number and instrument type
-            newNote.number = note.NoteNumber;
-            // note position
-            newNote.pos = note.TimeAs(TimeSpanType.BarBeatTicks, tempoMap).ToString();
-            // note length for press & hold
-            newNote.Length = note.Length;
-            // note types
-            newNote.Velocity = note.Velocity;
-            trackInfo.Append(newNote);
-        }
+
+        BinaryTrack = new uint[totalBars * BEATS_PER_BAR * SPOTS_PER_BEAT];
     }
 }
