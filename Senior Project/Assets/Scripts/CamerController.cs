67ffda46f4122eef3879d3fc919f478b7f42ccc6
GelzoneXUnsas <51806064+GelzoneXUnsas@users.noreply.github.com>
2022-11-08 14:04:13 -0800

    adding far background

--- Senior Project/Assets/Scripts/CamerController.cs
+++ Senior Project/Assets/Scripts/CamerController.cs
@@ -2,39 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CamerController : MonoBehaviour {
+public class CamerController : MonoBehaviour
+{
     public float speed;
+
     public bool isMoving;
+
     public GameObject staticBackground;
 
     public float cameraX;
 
+    public float farBackgroundSpeed;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         cameraX = transform.position.x;
-		isMoving = true;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        isMoving = true;
+        farBackgroundSpeed = 0.03f;
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
         if (Input.GetKey(KeyCode.S))
         {
             isMoving = false;
         }
-        if (Input.GetKey(KeyCode.D)){
+        if (Input.GetKey(KeyCode.D))
+        {
             isMoving = true;
         }
-/*        if (transform.position.x >= 6.0f)
+
+        /*        if (transform.position.x >= 6.0f)
         {
             transform.Translate(new Vector3(-28f, 0, 0));
         }*/
-        // if (isMoving)
-        // {
-        //     transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
-        //     staticBackground.transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
-        // }
+        if (isMoving)
+        {
+            staticBackground
+                .transform
+                .Translate(new Vector3(-speed *
+                    Time.deltaTime *
+                    farBackgroundSpeed,
+                    0,
+                    0));
+        }
         // if (Input.GetKey(KeyCode.Space))
         // {
         //     Debug.Log(cameraX);
