9164e38fafdb5d7509dd1c2d4650a42d911ba5a8
Alex Hartford <alexanderhartford@gmail.com>
2023-02-11 13:10:48 -0800

    fixed multi-notes

--- Senior Project/Assets/Scripts/Spawning/SpawnMaster.cs
+++ Senior Project/Assets/Scripts/Spawning/SpawnMaster.cs
@@ -10,6 +10,7 @@ public class SpawnMaster : MonoBehaviour
     public Spawner spawner4;
     public MIDIReader MIDIReader;
     public Conductor conductor;
+
     // Start is called before the first frame update
     void Start()
     {
