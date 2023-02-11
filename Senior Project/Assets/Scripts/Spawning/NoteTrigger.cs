766e5fa99aaba13e82d00caa5a68af13fd2c11b1
GelzoneXUnsas <51806064+GelzoneXUnsas@users.noreply.github.com>
2023-02-11 12:33:37 -0800

    fixing merge

--- Senior Project/Assets/Scripts/Spawning/NoteTrigger.cs
+++ Senior Project/Assets/Scripts/Spawning/NoteTrigger.cs
@@ -9,26 +9,38 @@ public enum HitCategory
     NONE,
     WEAK,
     GOOD
-};
+}
 
 public class NoteTrigger : MonoBehaviour
 {
     public Conductor conductor;
+
     public MIDIReader midiReader;
+
     public ParticleSystem particles;
+
     ParticleSystem.MainModule psmain;
+
     public ParticleSystem hittext;
+
     public PlayerController character;
+
     public TMPro.TextMeshProUGUI text;
+
     public Animator anim;
 
     public SpriteRenderer top;
+
     public SpriteRenderer high;
+
     public SpriteRenderer low;
+
     public SpriteRenderer bot;
 
     public Color perfect;
+
     public Color weak;
+
     public Color fail;
 
     public SFX sfx;
@@ -36,16 +48,23 @@ public class NoteTrigger : MonoBehaviour
     public ScoreManager scoreManager;
 
     public float currentSpot;
+
     public float noteEnd;
+
     public float innerThreshold;
+
     public float outerThreshold;
 
     private float lowerGoodBound;
+
     private float lowerWeakBound;
+
     private float upperGoodBound;
+
     private float upperWeakBound;
 
     int transpose = 0;
+
     int[] notes = new int[] { 7, 4, 0, -12 };
 
     [SerializeReference]
@@ -58,7 +77,7 @@ public class NoteTrigger : MonoBehaviour
     {
         currentSpot = conductor.spotLength;
         noteEnd = conductor.spotLength + conductor.spotLength * 0.5f;
-        hasBeenPressed = new bool[] { false, false, false, false};
+        hasBeenPressed = new bool[] { false, false, false, false };
         psmain = particles.main;
     }
 
@@ -68,23 +87,27 @@ public class NoteTrigger : MonoBehaviour
         if (checkHit(KeyCode.Joystick1Button3, 0, top, KeyCode.H))
             hasBeenPressed[0] = true;
 
-        if(checkHit(KeyCode.Joystick1Button2, 1, high, KeyCode.J))
+        if (checkHit(KeyCode.Joystick1Button2, 1, high, KeyCode.J))
             hasBeenPressed[1] = true;
 
-        if(checkHit(KeyCode.Joystick1Button0, 2, low, KeyCode.K))
+        if (checkHit(KeyCode.Joystick1Button0, 2, low, KeyCode.K))
             hasBeenPressed[2] = true;
 
-        if(checkHit(KeyCode.Joystick1Button1, 3, bot, KeyCode.L))
+        if (checkHit(KeyCode.Joystick1Button1, 3, bot, KeyCode.L))
             hasBeenPressed[3] = true;
 
         // Set the current Hit Category
-        if (conductor.songPosition >= lowerGoodBound &&
-            conductor.songPosition <= upperGoodBound)
+        if (
+            conductor.songPosition >= lowerGoodBound &&
+            conductor.songPosition <= upperGoodBound
+        )
         {
             hc = HitCategory.GOOD;
         }
-        else if (conductor.songPosition >= lowerWeakBound &&
-            conductor.songPosition <= upperWeakBound)
+        else if (
+            conductor.songPosition >= lowerWeakBound &&
+            conductor.songPosition <= upperWeakBound
+        )
         {
             hc = HitCategory.WEAK;
         }
@@ -93,79 +116,74 @@ public class NoteTrigger : MonoBehaviour
             hc = HitCategory.NONE;
         }
 
-<<<<<<< HEAD:Senior Project/Assets/Scripts/NoteTrigger.cs
         // Set the next beat when current beat is over.
         if (conductor.songPosition > noteEnd)
         {
             currentSpot += conductor.spotLength;
             noteEnd += conductor.spotLength;
-=======
-		// Set the next beat when current beat is over.
-		if (conductor.songPosition > noteEnd)
-		{
-			currentSpot += conductor.spotLength;
-			noteEnd += conductor.spotLength;
-			lowerGoodBound = currentSpot - innerThreshold;
-			lowerWeakBound = currentSpot - outerThreshold;
-			upperGoodBound = currentSpot + innerThreshold;
-			upperWeakBound = currentSpot + outerThreshold;
-			// BELOW: Consequences for missing things
-			switch(midiReader.pressable[0])
-			{
-				case NoteType.NOTE:
-				{
-					if (!hasBeenPressed[0])
-					{
-						ResolveMiss(top, 0);
-					}
-				} break;
-				case NoteType.OBSTACLE:
-				{
-					if (character.playerState == 2)
-					{
-							Debug.Log("Here1");
-						ResolveHitObstacle(top, 0);
-					}
-				} break;
-			}
-			switch(midiReader.pressable[1])
-			{
-				case NoteType.NOTE:
-				{
-					if (!hasBeenPressed[1])
-					{
-						ResolveMiss(high, 1);
-					}
-				} break;
-				case NoteType.OBSTACLE:
-				{
-					if (character.playerState == 1)
-					{
-							Debug.Log("Here2");
-							ResolveHitObstacle(high, 1);
-					}
-				} break;
-			}
-			switch(midiReader.pressable[2])
-			{
-				case NoteType.NOTE:
-				{
-					if (!hasBeenPressed[2])
-					{
-						ResolveMiss(low, 2);
-					}
-				} break;
-				case NoteType.OBSTACLE:
-				{
-					if (character.playerState == 0)
-					{
-							Debug.Log("Here3");
-							ResolveHitObstacle(low, 2);
-					}
-				} break;
+            lowerGoodBound = currentSpot - innerThreshold;
+            lowerWeakBound = currentSpot - outerThreshold;
+            upperGoodBound = currentSpot + innerThreshold;
+            upperWeakBound = currentSpot + outerThreshold;
 
-			}
->>>>>>> 0303ab39c6a513f44d0812cafe316b964872a0f8:Senior Project/Assets/Scripts/Spawning/NoteTrigger.cs
+            // BELOW: Consequences for missing things
+            switch (midiReader.pressable[0])
+            {
+                case NoteType.NOTE:
+                    {
+                        if (!hasBeenPressed[0])
+                        {
+                            ResolveMiss(top, 0);
+                        }
+                    }
+                    break;
+                case NoteType.OBSTACLE:
+                    {
+                        if (character.playerState == 2)
+                        {
+                            ResolveHitObstacle(top, 0);
+                        }
+                    }
+                    break;
+            }
+            switch (midiReader.pressable[1])
+            {
+                case NoteType.NOTE:
+                    {
+                        if (!hasBeenPressed[1])
+                        {
+                            ResolveMiss(high, 1);
+                        }
+                    }
+                    break;
+                case NoteType.OBSTACLE:
+                    {
+                        if (character.playerState == 1)
+                        {
+                            ResolveHitObstacle(high, 1);
+                        }
+                    }
+                    break;
+            }
+            switch (midiReader.pressable[2])
+            {
+                case NoteType.NOTE:
+                    {
+                        if (!hasBeenPressed[2])
+                        {
+                            ResolveMiss(low, 2);
+                        }
+                    }
+                    break;
+                case NoteType.OBSTACLE:
+                    {
+                        if (character.playerState == 0)
+                        {
+                            ResolveHitObstacle(low, 2);
+                        }
+                    }
+                    break;
+            }
 
             lowerGoodBound = currentSpot - innerThreshold;
             upperGoodBound = currentSpot + innerThreshold;
@@ -174,77 +192,81 @@ public class NoteTrigger : MonoBehaviour
             upperWeakBound = currentSpot + outerThreshold;
 
             // BELOW: Consequences for missing things
-            switch(midiReader.pressable[0])
+            switch (midiReader.pressable[0])
             {
                 case NoteType.NOTE:
-                {
-                    if (!hasBeenPressed[0])
                     {
-                        ResolveMiss(top, 0);
+                        if (!hasBeenPressed[0])
+                        {
+                            ResolveMiss(top, 0);
+                        }
                     }
-                } break;
+                    break;
                 case NoteType.OBSTACLE:
-                {
-                    if (character.playerState == 2)
                     {
-			Debug.Log("Here1");
-                        ResolveHitObstacle(top, 0);
+                        if (character.playerState == 2)
+                        {
+                            ResolveHitObstacle(top, 0);
+                        }
                     }
-                } break;
+                    break;
             }
-            switch(midiReader.pressable[1])
+            switch (midiReader.pressable[1])
             {
                 case NoteType.NOTE:
-                {
-                    if (!hasBeenPressed[1])
                     {
-                        ResolveMiss(high, 1);
+                        if (!hasBeenPressed[1])
+                        {
+                            ResolveMiss(high, 1);
+                        }
                     }
-                } break;
+                    break;
                 case NoteType.OBSTACLE:
-                {
-                    if (character.playerState == 1)
                     {
-                            Debug.Log("Here2");
+                        if (character.playerState == 1)
+                        {
                             ResolveHitObstacle(high, 1);
+                        }
                     }
-                } break;
+                    break;
             }
-            switch(midiReader.pressable[2])
+            switch (midiReader.pressable[2])
             {
                 case NoteType.NOTE:
-                {
-                    if (!hasBeenPressed[2])
                     {
-                        ResolveMiss(low, 2);
+                        if (!hasBeenPressed[2])
+                        {
+                            ResolveMiss(low, 2);
+                        }
                     }
-                } break;
+                    break;
                 case NoteType.OBSTACLE:
-                {
-                    if (character.playerState == 0)
                     {
-                            Debug.Log("Here3");
+                        if (character.playerState == 0)
+                        {
                             ResolveHitObstacle(low, 2);
+                        }
                     }
-                } break;
+                    break;
             }
-            switch(midiReader.pressable[3])
+            switch (midiReader.pressable[3])
             {
                 case NoteType.NOTE:
-                {
-                    if (!hasBeenPressed[3])
                     {
-                        ResolveMiss(bot, 3);
+                        if (!hasBeenPressed[3])
+                        {
+                            ResolveMiss(bot, 3);
+                        }
                     }
-                } break;
+                    break;
                 case NoteType.OBSTACLE:
-                {
-                    if (character.playerState == 0)
                     {
-                            Debug.Log("Here4");
+                        if (character.playerState == 0)
+                        {
                             ResolveHitObstacle(bot, 3);
+                        }
                     }
-                } break;
+                    break;
             }
 
             hasBeenPressed[0] = false;
@@ -255,16 +277,31 @@ public class NoteTrigger : MonoBehaviour
         }
     }
 
-    private void ResolveHit(HitCategory hc, SpriteRenderer sprite, int trackNumber)
+    private void ResolveHit(
+        HitCategory hc,
+        SpriteRenderer sprite,
+        int trackNumber
+    )
     {
         if (hc == HitCategory.WEAK)
         {
-            sfx.sounds[2].pitch = Mathf.Pow(2, (float)((notes[trackNumber] + transpose) / 12.0));
+            sfx.sounds[2].pitch =
+                Mathf.Pow(2, (float)((notes[trackNumber] + transpose) / 12.0));
             sfx.sounds[2].Play();
             psmain.startColor = weak;
-            ParticleSystem clone = (ParticleSystem)Instantiate(particles, sprite.transform.position, Quaternion.identity);
+            ParticleSystem clone =
+                (ParticleSystem)
+                Instantiate(particles,
+                sprite.transform.position,
+                Quaternion.identity);
             text.text = "Weak";
-            ParticleSystem clone2 = (ParticleSystem)Instantiate(hittext, new Vector3(sprite.transform.position.x, sprite.transform.position.y, -6), Quaternion.identity);
+            ParticleSystem clone2 =
+                (ParticleSystem)
+                Instantiate(hittext,
+                new Vector3(sprite.transform.position.x,
+                    sprite.transform.position.y,
+                    -6),
+                Quaternion.identity);
             sprite.color = weak;
             Destroy(clone.gameObject, 0.5f);
             Destroy(clone2.gameObject, 1.0f);
@@ -272,13 +309,24 @@ public class NoteTrigger : MonoBehaviour
         }
         else if (hc == HitCategory.GOOD)
         {
-            sfx.sounds[1].pitch = Mathf.Pow(2, (float)((notes[trackNumber] + transpose) / 12.0));
+            sfx.sounds[1].pitch =
+                Mathf.Pow(2, (float)((notes[trackNumber] + transpose) / 12.0));
             sfx.sounds[1].Play();
             perfect.a = 1f;
             psmain.startColor = perfect;
-            ParticleSystem clone = (ParticleSystem)Instantiate(particles, sprite.transform.position, Quaternion.identity);
+            ParticleSystem clone =
+                (ParticleSystem)
+                Instantiate(particles,
+                sprite.transform.position,
+                Quaternion.identity);
             text.text = "Perfect";
-            ParticleSystem clone2 = (ParticleSystem)Instantiate(hittext, new Vector3(sprite.transform.position.x, sprite.transform.position.y, -6), Quaternion.identity);
+            ParticleSystem clone2 =
+                (ParticleSystem)
+                Instantiate(hittext,
+                new Vector3(sprite.transform.position.x,
+                    sprite.transform.position.y,
+                    -6),
+                Quaternion.identity);
             perfect.a = 0.75f;
             sprite.color = perfect;
             Destroy(clone.gameObject, 0.5f);
@@ -290,28 +338,50 @@ public class NoteTrigger : MonoBehaviour
     private void ResolveMiss(SpriteRenderer sprite, int trackNumber)
     {
         anim.SetTrigger("hurt");
+
         // TODO (Alex): Should a miss incur a sound effect?
         //sfx.sounds[3].pitch = Mathf.Pow(2, (float)((notes[trackNumber] + transpose) / 12.0));
         //sfx.sounds[3].Play();
         psmain.startColor = fail;
-        ParticleSystem clone = (ParticleSystem)Instantiate(particles, sprite.transform.position, Quaternion.identity);
+        ParticleSystem clone =
+            (ParticleSystem)
+            Instantiate(particles,
+            sprite.transform.position,
+            Quaternion.identity);
         text.text = "Miss";
-        ParticleSystem clone2 = (ParticleSystem)Instantiate(hittext, new Vector3(sprite.transform.position.x, sprite.transform.position.y, -6), Quaternion.identity);
+        ParticleSystem clone2 =
+            (ParticleSystem)
+            Instantiate(hittext,
+            new Vector3(sprite.transform.position.x,
+                sprite.transform.position.y,
+                -6),
+            Quaternion.identity);
         sprite.color = fail;
         Destroy(clone.gameObject, 0.5f);
         Destroy(clone2.gameObject, 1.0f);
         scoreManager.score -= 2;
     }
-    
+
     private void ResolveHitObstacle(SpriteRenderer sprite, int trackNumber)
     {
         anim.SetTrigger("hurt");
-        sfx.sounds[3].pitch = Mathf.Pow(2, (float)((notes[trackNumber] + transpose) / 12.0));
+        sfx.sounds[3].pitch =
+            Mathf.Pow(2, (float)((notes[trackNumber] + transpose) / 12.0));
         sfx.sounds[3].Play();
         psmain.startColor = fail;
-        ParticleSystem clone = (ParticleSystem)Instantiate(particles, sprite.transform.position, Quaternion.identity);
+        ParticleSystem clone =
+            (ParticleSystem)
+            Instantiate(particles,
+            sprite.transform.position,
+            Quaternion.identity);
         text.text = "Miss";
-        ParticleSystem clone2 = (ParticleSystem)Instantiate(hittext, new Vector3(sprite.transform.position.x, sprite.transform.position.y, -6), Quaternion.identity);
+        ParticleSystem clone2 =
+            (ParticleSystem)
+            Instantiate(hittext,
+            new Vector3(sprite.transform.position.x,
+                sprite.transform.position.y,
+                -6),
+            Quaternion.identity);
         sprite.color = fail;
         Destroy(clone.gameObject, 0.5f);
         Destroy(clone2.gameObject, 1.0f);
@@ -320,13 +390,14 @@ public class NoteTrigger : MonoBehaviour
 
     // Checks for a hit on a given keycode.
     // Returns true if hit, false if not.
-    private bool checkHit(KeyCode kc, int trackNumber, SpriteRenderer sprite, KeyCode kb)
+    private bool
+    checkHit(KeyCode kc, int trackNumber, SpriteRenderer sprite, KeyCode kb)
     {
         if (Input.GetKeyDown(kc) || Input.GetKeyDown(kb))
         {
             if (midiReader.pressable[trackNumber] == NoteType.NOTE)
             {
-                ResolveHit(hc, sprite, trackNumber);
+                ResolveHit (hc, sprite, trackNumber);
                 return true;
             }
             else if (midiReader.pressable[trackNumber] == NoteType.OBSTACLE)
