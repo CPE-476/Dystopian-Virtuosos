1830e0c75244fe7732661672b75cf65860e229c6
Alex Hartford <alexanderhartford@gmail.com>
2023-02-11 12:17:33 -0800

    fixed dupe note bug + tweaks + stevie

--- Senior Project/Assets/Scripts/MIDIReader.cs
+++ Senior Project/Assets/Scripts/MIDIReader.cs
@@ -57,6 +57,7 @@ public class MIDIReader : MonoBehaviour
     List<NoteElement> trackInfo = new List<NoteElement>();
 
     public SpotElement[] SpotTrack;
+    public bool changed = false;
 
     /*
         xxxx  xxxx  xxxx  xxxx  xxxx  xxxx  xxxx  xxxx 
@@ -196,6 +197,7 @@ public class MIDIReader : MonoBehaviour
 
     public void changePressable()
     {
+        changed = true;
         index =
             conductor.barNumber *
             Conductor.BEATS_PER_BAR *
