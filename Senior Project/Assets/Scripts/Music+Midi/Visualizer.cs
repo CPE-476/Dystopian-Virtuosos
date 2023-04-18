fbf87f493f1f4aae3e5bf03ec08f22646d892ab3
bretthickman <gelzonexunsas@gmail.com>
2023-04-18 15:56:43 -0700

    added MV

--- Senior Project/Assets/Scripts/Music+Midi/Visualizer.cs
+++ Senior Project/Assets/Scripts/Music+Midi/Visualizer.cs
@@ -5,13 +5,12 @@ using UnityEngine.UI;
 
 public class Visualizer : MonoBehaviour
 {
-    public float minHeight = 15f;
-    public float maxHeight = 425f;
-    public float updateSenstivity = 0.2f;
-    public Color visualizerColor = Color.white;
+    public float minHeight;
+    public float maxHeight;
+    public float updateSenstivity;
+    public Color visualizerColor;
 
     [Space(12)]
-    public AudioClip audioCilp;
     public bool loop = true;
     [Space(12), Range(64, 8192)]
     public int visualizerSamples = 64;
@@ -24,13 +23,13 @@ public class Visualizer : MonoBehaviour
     {
         visualizerObjects = GetComponentsInChildren<VisualizerObject>();
 
-        if (!audioCilp)
-            return;
+        //if (!audioCilp)
+        //return;
 
-        m_audioSource = new GameObject("AudioSource").AddComponent<AudioSource>();
-        m_audioSource.loop = loop;
-        m_audioSource.clip = audioCilp;
-        m_audioSource.Play();
+        m_audioSource = GameObject.Find("Conductor").GetComponent<AudioSource>();
+        // m_audioSource.loop = loop;
+        // m_audioSource.clip = audioCilp;
+        //m_audioSource.Play();
 
     }
 
@@ -39,13 +38,25 @@ public class Visualizer : MonoBehaviour
     void Update()
     {
         float[] spectrumData = m_audioSource.GetSpectrumData(visualizerSamples, 0, FFTWindow.Rectangular);
+        float averageVolume = 0f;
+        for (int i = 0; i < spectrumData.Length; i++)
+        {
+            averageVolume += spectrumData[i];
+        }
+        averageVolume /= spectrumData.Length;
+
         for (int i = 0; i < visualizerObjects.Length; i++)
         {
-            Vector2 newSize = visualizerObjects[i].GetComponent<RectTransform>().rect.size;
+            Vector3 newSize = visualizerObjects[i].transform.localScale;
 
-            newSize.y = Mathf.Clamp(Mathf.Lerp(newSize.y, minHeight + (spectrumData[i] * (maxHeight - minHeight) * 5.0f), updateSenstivity), minHeight, maxHeight);
-            visualizerObjects[i].transform.localScale = new Vector3(newSize.x, newSize.y, 1f);
+            newSize.y = Mathf.Clamp(Mathf.Lerp(newSize.y, minHeight + (averageVolume * (maxHeight - minHeight) * 25.0f), updateSenstivity), minHeight, maxHeight);
+            visualizerObjects[i].transform.localScale = new Vector3(visualizerObjects[i].transform.localScale.x, newSize.y, visualizerObjects[i].transform.localScale.z);
             visualizerObjects[i].GetComponent<SpriteRenderer>().color = visualizerColor;
+
+            // Apply noise to the sprite height
+            float noise = Mathf.PerlinNoise(Time.time, i * 0.1f) * 0.5f - 0.1f;
+            newSize.y += noise;
+            visualizerObjects[i].transform.localScale = new Vector3(visualizerObjects[i].transform.localScale.x, newSize.y, visualizerObjects[i].transform.localScale.z);
         }
     }
 }
