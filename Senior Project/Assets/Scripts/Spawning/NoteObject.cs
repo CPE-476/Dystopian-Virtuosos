c5f7d8959b86c6a6e236a910d661b7229d35213f
GelzoneXUnsas <51806064+GelzoneXUnsas@users.noreply.github.com>
2023-05-29 14:02:14 -0700

    update for demo, can overwrite this

--- Senior Project/Assets/Scripts/Spawning/NoteObject.cs
+++ Senior Project/Assets/Scripts/Spawning/NoteObject.cs
@@ -149,6 +149,7 @@ public class NoteObject : MonoBehaviour
         {
             if(followers[0] != null)
             {
+                Destroy(gameObject);
                 Destroy(followers[0]);
                 Destroy(followers[1]);
                 Destroy(followers[2]);
