de9866eb5eb9943eb0f7bc5fcc04adf96ce2eed4
Lucas Li <gelzonexunsas@gmail.com>
2023-06-04 20:45:37 -0700

    spine refactor, streamingAsset added

--- Senior Project/Assets/Scripts/Spine.cs
+++ Senior Project/Assets/Scripts/Spine.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+//using UnityEngine.UIElements;
 using UnityEngine.Video;
 
 public enum InterfaceState
@@ -120,136 +121,35 @@ public class Spine : MonoBehaviour
 
     public Tutorial tutorial;
 
+    public string midiPath;
+
+    public string videoPath;
+
+    //public string dialoguePath;
+
     private Image image;
 
     private Color color;
 
-    public DialogueLine[]
-        first_dialogue =
-        {
-            new DialogueLine(Character.RIKA,
-                "Welcome to Virtuosos!"),
-            new DialogueLine(Character.RIKA,
-                "This is an early demo, so give whatever feedback you can. So let's get started!"),
-            new DialogueLine(Character.RIKA,
-                "There are two kinds of input: Keyboard and Controller."),
-            new DialogueLine(Character.RIKA,
-                "If you're using the controller, hit the low track with Left Bumper, and the high track with Right Bumper. Don't overthink it!"),
-            new DialogueLine(Character.RIKA,
-                "On Keyboard, L hits the low track, and K hits the high track."),
-            new DialogueLine(Character.RIKA,
-                "Watch out! Up ahead, looks like there's some obstacles."),
-            new DialogueLine(Character.RIKA,
-                "Rocks can hurt you, so avoid them."),
-            new DialogueLine(Character.RIKA,
-                "You'll want to hit the enemies on the beat, and if you hit them right, you get points!"),
-            new DialogueLine(Character.RIKA,
-                "Finally, if you see screws, try to collect them."),
-            new DialogueLine(Character.RIKA, "This is a rhythm game, so listen to the rhythms to know when to hit your targets."),
-            new DialogueLine(Character.RIKA, "Do your best!")
-        };
+    DialogueLine[] first_dialogue = new DialogueLine[0];
+    DialogueLine[] second_dialogue = new DialogueLine[0];
+    DialogueLine[] third_dialogue = new DialogueLine[0];
+    DialogueLine[] fourth_dialogue = new DialogueLine[0];
 
-    public DialogueLine[]
-        second_dialogue =
-        {
-            new DialogueLine(Character.RIKA,
-                "Well done! That was the drummer mode."),
-            new DialogueLine(Character.RIKA,
-                "Next, we'll switch to another game mode: piano."),
-            new DialogueLine(Character.RIKA,
-                "The piano mode has hold notes. When you see a hold note, try to hit it on time, and hold it for as long as its tail lasts."),
-            new DialogueLine(Character.RIKA,
-                "Keep in mind, you might need to hold notes while other notes are played, so stay sharp! (haha)"),
-            new DialogueLine(Character.RIKA,
-                "The other thing is that the piano mode has four tracks. On Controller, you hit them with the left and right joysticks. Try it out before the notes start coming!"),
-            new DialogueLine(Character.RIKA,
-                "If you're on keyboard, you'll input using H, J, K, and L."),
-            new DialogueLine(Character.RIKA,
-                "Alright, here they come! Good luck!")
-        };
-
-    public DialogueLine[]
-        third_dialogue =
-        {
-            new DialogueLine(Character.RIKA,
-                "Excellent!"),
-            new DialogueLine(Character.RIKA,
-                "Time for the final showdown. This time, we'll be in the last game mode: Guitar!"),
-            new DialogueLine(Character.RIKA,
-                "Guitar mode is a combination of piano and drum modes. There are three tracks, and you can encounter both hit and hold notes at the same time."),
-            new DialogueLine(Character.RIKA,
-                "On controller, use the Y, B, and A buttons to hit and hold notes."),
-            new DialogueLine(Character.RIKA,
-                "On keyboard, it's the same as piano, just no top track."),
-            new DialogueLine(Character.RIKA,
-                "Alright, here we go!")
-        };
-
-    public DialogueLine[]
-        fourth_dialogue = { new DialogueLine(Character.RIKA, "That's it.") };
-
-    public TutorialClip[]
-        first_tutorial = {
-            new TutorialClip("OBSTACLE",
-            "Assets/videos/test_1.mp4"),
-            new TutorialClip("NOTE",
-            "Assets/videos/test_2.mp4"),
-            new TutorialClip("CONTROL",
-            "Assets/videos/test_3.mp4"),
-        };
-
-    public TutorialClip[]
-        second_tutorial = {
-
-        };
-
-    public TutorialClip[]
-        third_tutorial = {
-
-        };
+    TutorialClip[] first_tutorial = new TutorialClip[0];
+    TutorialClip[] second_tutorial = new TutorialClip[0];
+    TutorialClip[] third_tutorial = new TutorialClip[0];
+    void Awake()
+    {
+        midiPath = Application.streamingAssetsPath + "/MIDIs/";
+        videoPath = Application.streamingAssetsPath + "/videos/";
+    }
 
     void Start()
     {
-        sections =
-            new Section[6]
-            {
-                new Section(first_dialogue, first_tutorial,
-                    "Assets/Music/DV_L2_Section_1.mid",
-                    GameplayAudio.S1,
-                    1,
-                    false,
-                    -17),
-                new Section(first_dialogue, first_tutorial,
-                    "Assets/Music/DV_L2_Section_2.mid",
-                    GameplayAudio.S2,
-                    2,
-                    false,
-                    -1),
-                new Section(first_dialogue, first_tutorial,
-                    "Assets/Music/DV_L2_Section_2.mid",
-                    GameplayAudio.END,
-                    3,
-                    false,
-                    -17),
-                new Section(first_dialogue, first_tutorial,
-                    "Assets/Music/DV_L1_drum.mid",
-                    GameplayAudio.DRUMS,
-                    0,
-                    true,
-                    -1),
-                new Section(second_dialogue, second_tutorial,
-                    "Assets/Music/DV_L1_piano.mid",
-                    GameplayAudio.PIANO,
-                    0,
-                    false,
-                    -17),
-                new Section(third_dialogue, third_tutorial,
-                    "Assets/Music/DV_L1_guitar.mid",
-                    GameplayAudio.GUITAR,
-                    0,
-                    true,
-                    -17),
-            };
+        initDialogue();
+        initTutorial();
+        initSection();
 
         image = fade.GetComponent<Image>();
         color = image.color;
@@ -275,11 +175,11 @@ public class Spine : MonoBehaviour
         if (sections[section_index].background_audio)
             conductor.playBackground = true;
 
-        if(sections[section_index].l2_background_audio == 1)
+        if (sections[section_index].l2_background_audio == 1)
             conductor.playL2BG1 = true;
-        if(sections[section_index].l2_background_audio == 2)
+        if (sections[section_index].l2_background_audio == 2)
             conductor.playL2BG2 = true;
-        if(sections[section_index].l2_background_audio == 3)
+        if (sections[section_index].l2_background_audio == 3)
             conductor.playL2BG3 = true;
 
         state = InterfaceState.DIALOGUE;
@@ -290,7 +190,7 @@ public class Spine : MonoBehaviour
     }
 
     public void TutorialStart()
-    {   
+    {
         state = InterfaceState.TUTORIAL;
         tutorial.Enable();
         dialogue.Disable();
@@ -346,13 +246,13 @@ public class Spine : MonoBehaviour
         else
             conductor.playBackground = false;
 
-        if(sections[section_index].l2_background_audio == 1)
+        if (sections[section_index].l2_background_audio == 1)
             conductor.playL2BG1 = true;
 
-        if(sections[section_index].l2_background_audio == 2)
+        if (sections[section_index].l2_background_audio == 2)
             conductor.playL2BG2 = true;
 
-        if(sections[section_index].l2_background_audio == 3)
+        if (sections[section_index].l2_background_audio == 3)
             conductor.playL2BG3 = true;
 
         noteTrigger.Reset();
@@ -360,5 +260,102 @@ public class Spine : MonoBehaviour
         cam.isMoving = true;
 
         ++section_index;
+    }
+
+    private void initDialogue()
+    {
+        List<DialogueLine> dialogueList_1 = new List<DialogueLine>(first_dialogue);
+
+        dialogueList_1.Add(new DialogueLine(Character.RIKA,
+                            "Welcome to Virtuosos!"));
+        dialogueList_1.Add(new DialogueLine(Character.RIKA,
+                            "This is an early demo, so give whatever feedback you can. So let's get started!"));
+        dialogueList_1.Add(new DialogueLine(Character.RIKA,
+                            "There are two kinds of input: Keyboard and Controller."));
+
+        first_dialogue = dialogueList_1.ToArray();
+
+        List<DialogueLine> dialogueList_2 = new List<DialogueLine>(second_dialogue);
+
+        dialogueList_2.Add(new DialogueLine(Character.RIKA,
+                            "Well done! That was the drummer mode."));
+        dialogueList_2.Add(new DialogueLine(Character.RIKA,
+                            "Next, we'll switch to another game mode: piano."));
+        dialogueList_2.Add(new DialogueLine(Character.RIKA,
+                            "The piano mode has hold notes. When you see a hold note, try to hit it on time, and hold it for as long as its tail lasts."));
+
+        second_dialogue = dialogueList_2.ToArray();
+
+        List<DialogueLine> dialogueList_3 = new List<DialogueLine>(third_dialogue);
+
+        dialogueList_3.Add(new DialogueLine(Character.RIKA,
+                            "Excellent!"));
+        dialogueList_3.Add(new DialogueLine(Character.RIKA,
+                            "Time for the final showdown. This time, we'll be in the last game mode: Guitar!"));
+        dialogueList_3.Add(new DialogueLine(Character.RIKA,
+                "Guitar mode is a combination of piano and drum modes. There are three tracks, and you can encounter both hit and hold notes at the same time."));
+
+        third_dialogue = dialogueList_3.ToArray();
+
+    }
+
+    private void initTutorial()
+    {
+        List<TutorialClip> tutorialList_1 = new List<TutorialClip>(first_tutorial);
+
+        tutorialList_1.Add(new TutorialClip("OBSTACLE",
+                               videoPath + "test_1.mp4"));
+        tutorialList_1.Add(new TutorialClip("NOTE",
+                            videoPath + "test_2.mp4"));
+        tutorialList_1.Add(new TutorialClip("CONTROL",
+                            videoPath + "test_3.mp4"));
+
+        first_tutorial = tutorialList_1.ToArray();
+
+
+    }
+
+    private void initSection()
+    {
+        sections =
+            new Section[6]
+            {
+                new Section(first_dialogue, first_tutorial,
+                    midiPath + "DV_L2_Section_1.mid",
+                    GameplayAudio.S1,
+                    1,
+                    false,
+                    -17),
+                new Section(first_dialogue, first_tutorial,
+                    midiPath + "DV_L2_Section_2.mid",
+                    GameplayAudio.S2,
+                    2,
+                    false,
+                    -1),
+                new Section(first_dialogue, first_tutorial,
+                    midiPath + "DV_L2_Section_2.mid",
+                    GameplayAudio.END,
+                    3,
+                    false,
+                    -17),
+                new Section(first_dialogue, first_tutorial,
+                    midiPath + "DV_L1_drum.mid",
+                    GameplayAudio.DRUMS,
+                    0,
+                    true,
+                    -1),
+                new Section(second_dialogue, second_tutorial,
+                    midiPath + "DV_L1_piano.mid",
+                    GameplayAudio.PIANO,
+                    0,
+                    false,
+                    -17),
+                new Section(third_dialogue, third_tutorial,
+                    midiPath + "DV_L1_guitar.mid",
+                    GameplayAudio.GUITAR,
+                    0,
+                    true,
+                    -17),
+            };
     }
 }
