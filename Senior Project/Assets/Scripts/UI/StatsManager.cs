0dd55de20092c77fdbbaca7be5b2f47c6311b768
Lucas Li <gelzonexunsas@gmail.com>
2023-06-16 16:43:12 -0700

    stats grades added

--- Senior Project/Assets/Scripts/UI/StatsManager.cs
+++ Senior Project/Assets/Scripts/UI/StatsManager.cs
@@ -21,6 +21,15 @@ public class StatsManager : MonoBehaviour
     public TextMeshProUGUI collectText;
     public TextMeshProUGUI maxcomboText;
     public TextMeshProUGUI accuracyText;
+    public Image grade;
+
+    public Sprite S;
+    public Sprite A;
+    public Sprite B;
+    public Sprite C;
+    public Sprite D;
+    public Sprite F;
+
 
     public float onChangePannelRatio;
     public float onChangeLeftRatio;
@@ -36,22 +45,6 @@ public class StatsManager : MonoBehaviour
         targetLeftPos = Screen.width * onChangeLeftRatio;
         targetRightPos = Screen.width * onChangeRightRatio;
         //DisableStats();
-    }
-
-
-    void Update()
-    {
-        scoreText.text = scoreManager.score.ToString();
-        perfectText.text = noteTrigger.perfectNum.ToString();
-        goodText.text = noteTrigger.goodNum.ToString();
-        missText.text = noteTrigger.missNum.ToString();
-        collectText.text = Collectable.collectableNum.ToString() + " / 10";
-        maxcomboText.text = noteTrigger.maxCombo.ToString();
-        accuracyText.text = noteTrigger.accuracy.ToString("0.#") + " %";
-        if (Input.GetKeyDown(KeyCode.Return)) 
-        {
-            StartCoroutine(displayStatsUI(0.5f, 0.5f, 0.5f, 0.0f));
-        }
     }
 
     public void EnableStats()
@@ -154,10 +147,64 @@ public class StatsManager : MonoBehaviour
         }
     }
 
+    public IEnumerator displayGrade(float duration)
+    {
+        float timer = 0f;
+
+        while (timer < duration)
+        {
+            timer += Time.deltaTime;
+
+            float t = Mathf.Clamp01(timer / duration);
+
+
+                Vector3 startRightPosition = gameObject.transform.GetChild(9).transform.position;
+                Vector3 endRightPosition = new Vector3(targetRightPos + 150.0f,
+                                                    gameObject.transform.GetChild(9).transform.position.y,
+                                                    startRightPosition.z);
+                gameObject.transform.GetChild(9).transform.position = Vector3.Lerp(startRightPosition, endRightPosition, t);
+
+
+            yield return null;
+        }
+    }
+
 
 
     public IEnumerator displayStatsUI(float t1, float t2, float t3, float t4)
     {
+        scoreText.text = scoreManager.score.ToString();
+        perfectText.text = noteTrigger.perfectNum.ToString();
+        goodText.text = noteTrigger.goodNum.ToString();
+        missText.text = noteTrigger.missNum.ToString();
+        collectText.text = Collectable.collectableNum.ToString() + " / 10";
+        maxcomboText.text = noteTrigger.maxCombo.ToString();
+        accuracyText.text = noteTrigger.accuracy.ToString("0.#") + " %";
+        if (noteTrigger.accuracy >= 95) 
+        {
+            grade.sprite = S;
+        }
+        else if (noteTrigger.accuracy >= 90)
+        {
+            grade.sprite = A;
+        }
+        else if (noteTrigger.accuracy >= 85)
+        {
+            grade.sprite = B;
+        }
+        else if (noteTrigger.accuracy >= 80)
+        {
+            grade.sprite = C;
+        }
+        else if (noteTrigger.accuracy >= 75)
+        {
+            grade.sprite = D;
+        }
+        else
+        {
+            grade.sprite = F;
+        }
+
         yield return new WaitForSeconds(t4);
         StartCoroutine(displayPannel(t1));
         yield return new WaitForSeconds(t1);
@@ -165,5 +212,7 @@ public class StatsManager : MonoBehaviour
         yield return new WaitForSeconds(t2);
         StartCoroutine(displayLeft(t3));
         yield return new WaitForSeconds(t3);
+        StartCoroutine(displayGrade(t1));
+        yield return new WaitForSeconds(t1);
     }
 }
